c018c0c972ee4884d80f5a08fceba3eeb936dc5c
thongster <106802301+thongster@users.noreply.github.com>
2025-11-12 18:51:57 +0700

    eslint and prettier setup

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginPrettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      sourceType: "module",
+      globals: globals.node,
+    },
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    extends: [js.configs.recommended],
+    rules: {
+      // Enable Prettier inside ESLint
+      "prettier/prettier": "error",
+      // Optional extra style rules
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+]);
